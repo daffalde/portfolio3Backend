5ec9e1af7ae112a3b5e71845dbed2ca49a90e801
daffalde <daffalderayhan@gmail.com>
2025-06-01 23:39:29 +0700

    perbaikan pada routeInfo

--- src/route/routeInfo.js
+++ src/route/routeInfo.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { getData, insertUpdate } from "../controller/infoController.js";
 import { authToken } from "../middleware/middleware.js";
+import { upload } from "../middleware/multer.js";
 
 export const routeInfo = express.Router();
 
